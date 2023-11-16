01eae76ab83540de738c0a58924da39b53fc728d
philc@tickboxsystems.com <philc@tickboxsystems.com>
2023-11-16 10:37:13 +0000

    Created ServerModule1, Tickbox Systems Ltd Logo 140x39.gif.png and frmFeedback; Edited settings; Deleted Form1

---
+++ client_code/frmFeedback/__init__.py
@@ -0,0 +1,33 @@
+from ._anvil_designer import frmFeedbackTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class frmFeedback(frmFeedbackTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def btnSubmit_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # Set 'name' to the text in the 'name_box'
+    name = self..text
+    # Set 'email' to the text in the 'email_box'
+    email = self.email_box.text
+    # Set 'feedback' to the text in the 'feedback_box'
+    feedback = self.feedback_box.text
+    # Call your 'add_feedback' server function
+    # pass in name, email and feedback as arguments
+    anvil.server.call('add_feedback', name, email, feedback)
+    Notification("Tickbox Feedback submitted!").show()
+    self.clear_inputs()
+
+  def clear_inputs(self):
+    # Clear our three text boxes
+    self.name_box.text = ""
+    self.email_box.text = ""
+    self.feedback_box.text = ""
